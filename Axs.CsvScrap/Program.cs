30a3c7f5779fd39efb03967868850de3f1e9c6ec
dvariah <denys_variah@epam.com>
2023-09-03 20:23:47 +0300

    Call from main

--- Axs.CsvScrap/Program.cs
+++ Axs.CsvScrap/Program.cs
@@ -1,1 +1,45 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Axs.CsvScrap;
+
+class Program
+{
+    static void Main(String[] Args)
+    {
+        var reader = new Reader();
+        var helper = new ArgHelper();
+
+        helper.Parse(Args);
+
+        if (helper.isCallForHelp)
+        {
+            Console.WriteLine(ArgHelper.DOC);
+        }
+        if (helper.getUniqueTransactionIds)
+        {
+            var result = new List<string>();
+            foreach (var file in helper.Files)
+            {
+                var fields = reader.ReadField(file, 2).Result;
+                result.AddRange(fields);
+            }
+            foreach (var r in result)
+            {
+                Console.WriteLine(r);
+            }
+        }
+        if (helper.returnAssosiatedOrders)
+        {
+            var result = new List<string>();
+            foreach (var file in helper.Files)
+            {
+                var csvLines = reader.ReadWhereFieldEquals(file, 2, helper.Fields.First()).Result;
+                result.AddRange(csvLines);
+                Console.WriteLine($"here {csvLines.Count}");
+            }
+            foreach (var r in result)
+            {
+                Console.WriteLine(r);
+            }
+        }
+    }
+}
+
