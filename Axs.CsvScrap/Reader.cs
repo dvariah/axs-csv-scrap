25ddf930a64bf82e26404b819376775e2ebf99fa
dvariah <denys_variah@epam.com>
2023-09-05 23:20:58 +0300

    Read file from gzip archive

--- Axs.CsvScrap/Reader.cs
+++ Axs.CsvScrap/Reader.cs
@@ -1,4 +1,6 @@
-﻿namespace Axs.CsvScrap
+﻿using System.IO.Compression;
+
+namespace Axs.CsvScrap
 {
     public class Reader : IReader
     {
@@ -39,8 +41,119 @@
             return result;
         }
 
+        public async Task<List<string>> ReadCsvLineWhereFieldEquals(string filePath, Dictionary<int, string> idxValuePair)
+        {
+            var result = new List<string>();
+            using var fileStream = File.OpenRead(filePath);
+            using var gzStream = new GZipStream(fileStream, CompressionMode.Decompress);
+            using var reader = new StreamReader(gzStream);
+
+            var csvLine = await reader.ReadLineAsync();
+
+            while (csvLine != null)
+            {
+                var hit = true;
+
+                foreach (var kvp in idxValuePair)
+                {
+                    var field = GetField(csvLine, kvp.Key);
+                    if (field.Contains(kvp.Value)) { hit = false; break; }
+                }
+
+                if (hit) { result.Add(csvLine); }
+
+                csvLine = await reader.ReadLineAsync();
+            }
+
+            return result;
+        }
+
+        public async Task<List<string>> ReadUniqueFieldFromCsvFile(string filePath, int idx)
+        {
+            var result = new List<string>();
+            using var fileStream = File.OpenRead(filePath);
+            using var gzStream = new GZipStream(fileStream, CompressionMode.Decompress);
+            using var reader = new StreamReader(gzStream);
+
+            var csvLine = await reader.ReadLineAsync();
+
+            while (csvLine != null)
+            {
+                var field = GetField(csvLine, idx);
+                if (!result.Contains(field)) { result.Add(field); }
+                csvLine = await reader.ReadLineAsync();
+            }
+
+            return result;
+        }
+
+        public List<Sale> ReadSales(string filePath)
+        {
+            var result = new List<Sale>();
+            using var fileStream = File.OpenRead(filePath);
+            using var gzStream = new GZipStream(fileStream, CompressionMode.Decompress);
+            using var reader = new StreamReader(gzStream);
+
+            var csvLine = reader.ReadLine();
+
+            while (csvLine != null)
+            {
+                var sale = new Sale();
+                sale.unique_id = GetField(csvLine, 0);
+                sale.transaction_id = GetField(csvLine, 1);
+                sale.inventory_type = GetField(csvLine, 24);
+                result.Add(sale);
+                csvLine = reader.ReadLine();
+            }
+
+            return result;
+        }
+
+        public List<Payment> ReadPayments(string filePath)
+        {
+            var result = new List<Payment>();
+            using var fileStream = File.OpenRead(filePath);
+            using var gzStream = new GZipStream(fileStream, CompressionMode.Decompress);
+            using var reader = new StreamReader(gzStream);
+
+            var csvLine = reader.ReadLine();
+
+            while (csvLine != null)
+            {
+                var payment = new Payment();
+                payment.unique_id = GetField(csvLine, 0);
+                payment.transaction_id = GetField(csvLine, 1);
+                payment.payment_id = GetField(csvLine, 7);
+                result.Add(payment);
+                csvLine = reader.ReadLine();
+            }
+
+            return result;
+        }
+
+        public List<Distribution> ReadDistributions(string filePath)
+        {
+            var result = new List<Distribution>();
+            using var fileStream = File.OpenRead(filePath);
+            using var gzStream = new GZipStream(fileStream, CompressionMode.Decompress);
+            using var reader = new StreamReader(gzStream);
+
+            var csvLine = reader.ReadLine();
+
+            while (csvLine != null)
+            {
+                var distribution = new Distribution();
+                distribution.unique_id = GetField(csvLine, 0);
+                distribution.payment_id = GetField(csvLine, 6);
+                result.Add(distribution);
+                csvLine = reader.ReadLine();
+            }
+
+            return result;
+        }
+
         /// <summary>
-        /// Returns CSV element at index postion
+        /// Returns CSV element from CSV line at index postion
         /// </summary>
         /// <param name="s">Input csv string</param>
         /// <param name="i">Index of looked forcsv element</param>
