119dbeac860660d1298ae777c50d52cb7eb70ec8
dvariah <denys_variah@epam.com>
2023-09-12 10:38:34 +0300

    Compress result files into gz

--- Axs.CsvScrap/ResultFileWriter.cs
+++ Axs.CsvScrap/ResultFileWriter.cs
@@ -1,4 +1,6 @@
-﻿namespace Axs.CsvScrap
+﻿using System.IO.Compression;
+
+namespace Axs.CsvScrap
 {
     public class ResultFileWriter
     {
@@ -22,39 +24,54 @@
         public static void WriteExtractedSalesFile(string filePath, List<Sale> extractedSales)
         {
             ConsoleTracer.TraceMethodEntry($"Enter WriteExtractedSalesFile method. FilePath {filePath}, extractedSales count: {extractedSales.Count}");
+
             Directory.CreateDirectory(Path.GetDirectoryName(filePath));
             DeleteFileIfExists(filePath);
-            using var writer = File.CreateText(filePath);
+
+            using var file = File.Create(filePath);
+            using var compress = new GZipStream(file, CompressionMode.Compress);
+            using var writer = new StreamWriter(compress);
             foreach (var sale in extractedSales)
             {
                 writer.WriteLine(sale.OriginalCsvLine);
             }
+
             ConsoleTracer.TraceMethodEntry($"Exit WriteExtractedSalesFile method.");
         }
 
         public static void WriteExtractedPamentsFile(string filePath, List<Payment> extractedPayments)
         {
             ConsoleTracer.TraceMethodEntry($"Enter WriteExtractedPamentsFile method. FilePath {filePath}, extractedPayments count: {extractedPayments.Count}");
+
             Directory.CreateDirectory(Path.GetDirectoryName(filePath));
             DeleteFileIfExists(filePath);
-            using var writer = File.CreateText(filePath);
+
+            using var file = File.Create(filePath);
+            using var compress = new GZipStream(file, CompressionMode.Compress);
+            using var writer = new StreamWriter(compress);
             foreach (var payment in extractedPayments)
             {
                 writer.WriteLine(payment.OriginalCsvLine);
             }
+
             ConsoleTracer.TraceMethodEntry($"Exit WriteExtractedPamentsFile method.");
         }
 
         public static void WriteExtractedDistributionFile(string filePath, List<Distribution> extractedDistributions)
         {
             ConsoleTracer.TraceMethodEntry($"Enter WriteExtractedDistributionFile method. FilePath {filePath}, extractedDistributions count: {extractedDistributions.Count}");
+
             Directory.CreateDirectory(Path.GetDirectoryName(filePath));
             DeleteFileIfExists(filePath);
-            using var writer = File.CreateText(filePath);
+
+            using var file = File.Create(filePath);
+            using var compress = new GZipStream(file, CompressionMode.Compress);
+            using var writer = new StreamWriter(compress);
             foreach (var distribution in extractedDistributions)
             {
                 writer.WriteLine(distribution.OriginalCsvLine);
             }
+
             ConsoleTracer.TraceMethodEntry($"Exit WriteExtractedDistributionFile method.");
         }
 
