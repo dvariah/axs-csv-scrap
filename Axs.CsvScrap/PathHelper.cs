eef18a62c8379915cc4182b6a8899692dae9246f
dvariah <denys_variah@epam.com>
2023-09-12 10:38:07 +0300

    Chage result file path to gz format

--- Axs.CsvScrap/PathHelper.cs
+++ Axs.CsvScrap/PathHelper.cs
@@ -4,17 +4,17 @@
     {
         public static string CreateExtractedSalesFilePath(string workFolderPath, string countryCode, string cityName, string code)
         {
-            return $"{workFolderPath}\\input\\sales\\{countryCode}\\{cityName}\\axs_sales_{countryCode}_{cityName}_{code}.csv";
+            return $"{workFolderPath}\\input\\sales\\{countryCode}\\{cityName}\\axs_sales_{countryCode}_{cityName}_{code}.csv.gz";
         }
 
         public static string CreateExtractedPaymentsFilePath(string workFolderPath, string countryCode, string cityName, string code)
         {
-            return $"{workFolderPath}\\input\\payment\\{countryCode}\\{cityName}\\axs_payment_{countryCode}_{cityName}_{code}.csv";
+            return $"{workFolderPath}\\input\\payment\\{countryCode}\\{cityName}\\axs_payment_{countryCode}_{cityName}_{code}.csv.gz";
         }
 
         public static string CreateExtractedDistributionsFilePath(string workFolderPath, string countryCode, string cityName, string code)
         {
-            return $"{workFolderPath}\\input\\payment_distribution\\{countryCode}\\{cityName}\\axs_payment_distribution_{countryCode}_{cityName}_{code}.csv";
+            return $"{workFolderPath}\\input\\payment_distribution\\{countryCode}\\{cityName}\\axs_payment_distribution_{countryCode}_{cityName}_{code}.csv.gz";
         }
     }
 }
---
+++ Axs.CvsScrap.Tests/PathHelperTests.cs
@@ -0,0 +1,38 @@
+﻿using Axs.CsvScrap;
+using NUnit.Framework;
+
+namespace Axs.CvsScrap.Tests
+{
+    [TestFixture]
+    internal class PathHelperTests
+    {
+        PathHelper _sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _sut = new PathHelper();
+        }
+
+        [Test]
+        public void CreateExtractedSalesFilePath_ReturnsCorrectPath()
+        {
+            var result = PathHelper.CreateExtractedSalesFilePath("C:\\testFolder", "de", "pines", "42");
+            Assert.AreEqual($"C:\\testFolder\\input\\sales\\de\\pines\\axs_sales_de_pines_42.csv.gz", result);
+        }
+
+        [Test]
+        public void CreateExtractedPaymentsFilePath_ReturnsCorrectPath()
+        {
+            var result = PathHelper.CreateExtractedPaymentsFilePath("C:\\testFolder", "de", "pines", "42");
+            Assert.AreEqual($"C:\\testFolder\\input\\payment\\de\\pines\\axs_payment_de_pines_42.csv.gz", result);
+        }
+
+        [Test]
+        public void CreateExtracteddistributionsFilePath_ReturnsCorrectPath()
+        {
+            var result = PathHelper.CreateExtractedDistributionsFilePath("C:\\testFolder", "de", "pines", "42");
+            Assert.AreEqual($"C:\\testFolder\\input\\payment_distribution\\de\\pines\\axs_payment_distribution_de_pines_42.csv.gz", result);
+        }
+    }
+}
