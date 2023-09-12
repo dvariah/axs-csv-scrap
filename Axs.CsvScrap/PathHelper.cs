56b70a2937131029815b041cdcbcc92f375e96f7
dvariah <denys_variah@epam.com>
2023-09-12 16:14:38 +0300

    Should be txt format

--- Axs.CsvScrap/PathHelper.cs
+++ Axs.CsvScrap/PathHelper.cs
@@ -19,17 +19,17 @@
 
         public static string CreateControlCardSalesFilePath(string workFolderPath, string countryCode, string cityName, string code)
         {
-            return $"{workFolderPath}\\input\\sales\\{countryCode}\\{cityName}\\axs_sales_{countryCode}_{cityName}_{code}_control_card.csv.gz";
+            return $"{workFolderPath}\\input\\sales\\{countryCode}\\{cityName}\\axs_sales_{countryCode}_{cityName}_{code}_control_card.txt";
         }
 
         public static string CreateControlCardPaymentsFilePath(string workFolderPath, string countryCode, string cityName, string code)
         {
-            return $"{workFolderPath}\\input\\payment\\{countryCode}\\{cityName}\\axs_payment_{countryCode}_{cityName}_{code}_control_card.csv.gz";
+            return $"{workFolderPath}\\input\\payment\\{countryCode}\\{cityName}\\axs_payment_{countryCode}_{cityName}_{code}_control_card.txt";
         }
 
         public static string CreateControlCardDistributionsFilePath(string workFolderPath, string countryCode, string cityName, string code)
         {
-            return $"{workFolderPath}\\input\\payment_distribution\\{countryCode}\\{cityName}\\axs_payment_distribution_{countryCode}_{cityName}_{code}_control_card.csv.gz";
+            return $"{workFolderPath}\\input\\payment_distribution\\{countryCode}\\{cityName}\\axs_payment_distribution_{countryCode}_{cityName}_{code}_control_card.txt";
         }
     }
 }
